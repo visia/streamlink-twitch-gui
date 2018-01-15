19e1e5024d9efe2880f50b7ed5907462af01e7ab
bastimeyer <mail@bastimeyer.de>
2018-01-15 10:09:04 +0100

    i18n/en: SettingsChat

--- src/app/controllers/SettingsChatController.js
+++ src/app/controllers/SettingsChatController.js
@@ -26,9 +26,10 @@ export default Controller.extend({
 	providersDropDown: computed(function() {
 		const list = [];
 		for ( const [ id ] of providers ) {
-			const { label, exec } = chatConfig[ id ];
+			const { exec } = chatConfig[ id ];
 			if ( exec instanceof Object && !exec[ platform ] ) { continue; }
-			list.push({ id, label });
+			// TODO: remove label property after rewriting DropDownComponent
+			list.push({ id, label: id });
 		}
 		return list;
 	}),
